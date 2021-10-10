49fff86e91016591eaf1a72468781c9b50b32220
nancyR9 <91512653+nancyR9@users.noreply.github.com>
2021-10-10 10:10:16 -0500

    Add files via upload
    Operaciones completas cliente y mensaje

--- RETO_DOMINGO/js/misFunciones.js
+++ RETO_DOMINGO/js/misFunciones.js
@@ -10,6 +10,26 @@ function consultaInfoCliente(){
     });
 }
 
+function borrarElemento(idElemento){
+    let myData={
+        id:idElemento
+    };
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"https://g2ea280c58733a1-dbgrupo281111.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/client/client",
+        type:"DELETE",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            consultaInfoCliente();
+            alert("Se ha Eliminado.")
+        }
+    });
+}
+
+
 function consultaMensaje(){
     $.ajax({
         url:"https://g2ea280c58733a1-dbgrupo281111.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/message/message",
@@ -47,23 +67,92 @@ function registrarCliente(){
     });
 }
 
-function InsertarMensaje(){
-    let datosMensaje={
-        id:$("#id").val(),
-        messagetext:$("#descMensaje").val()
+function registrarMensaje(){
+    let Datos={
+        id:$("#id1").val(),
+        messagetext:$("#descMensaje").val(),
     };
-    let dataToSend=JSON.stringify(datosMensaje);
+    let dataToSend=JSON.stringify(Datos);
     $.ajax({
         url:"https://g2ea280c58733a1-dbgrupo281111.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/message/message",
         type:"POST",
-        data:datosMensaje,
+        data:Datos,
+        datatype:"JSON",
+        success:function(resultadoMensaje){
+            $("#resultadoMensaje").empty();
+            $("#id1").val("");
+            $("#descMensaje").val("");
+            alert("Se ha registro el mensaje")
+        }
+    });
+}
+
+function actualizarInformacion(){
+    let myData={
+        id:$("#id").val(),
+        name:$("#Nombre").val(),
+        email:$("#Email").val(),
+        age:$("#age").val(),
+
+    };
+    console.log(myData);
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"https://g2ea280c58733a1-dbgrupo281111.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/client/client",
+        type:"PUT",
+        data:dataToSend,
+        contentType:"application/JSON",
         datatype:"JSON",
         success:function(respuesta){
             $("#resultado").empty();
             $("#id").val("");
+            $("#Nombre").val("");
+            $("#Email").val("");
+            $("#age").val("");
+            consultaInfoCliente();
+            alert("se ha Actualizado")
+        }
+    });
+}
+
+function editarInformacionMensaje(){
+    let myData={
+        id:$("#id1").val(),
+        messagetext:$("#descMensaje").val()
+    };
+    console.log(myData);
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"https://g2ea280c58733a1-dbgrupo281111.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/message/message",
+        type:"PUT",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(resultadoMensaje){
+            $("#resultadoMensaje").empty();
+            $("#id1").val("");
             $("#descMensaje").val("");
             consultaMensaje();
-            alert("se ha guardado el dato")
+            alert("se ha Actualizado")
+        }
+    });
+}
+
+function borrarElementoMensaje(idElemento){
+    let myData={
+        id:idElemento
+    };
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"https://g2ea280c58733a1-dbgrupo281111.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/message/message",
+        type:"DELETE",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(resultadoMensaje){
+            $("#resultadoMensaje").empty();
+            consultaMensaje();
+            alert("Se ha Eliminado.")
         }
     });
 }
@@ -76,6 +165,7 @@ function respuestaCliente(items){
         tablaCliente+="<td>"+items[i].name+"</td>";
         tablaCliente+="<td>"+items[i].email+"</td>";
         tablaCliente+="<td>"+items[i].age+"</td>";
+        tablaCliente+="<td> <button onclick='borrarElemento("+items[i].id+")'>Borrar</button>";
         tablaCliente+="</tr>";
     }
     tablaCliente+="</table>";
@@ -102,36 +192,70 @@ function respuestaMensaje(items){
         tablaMensaje+="<tr>";
         tablaMensaje+="<td>"+items[i].id+"</td>";
         tablaMensaje+="<td>"+items[i].messagetext+"</td>";
+        tablaMensaje+="<td> <button onclick='borrarElementoMensaje("+items[i].id+")'>Borrar Mensaje</button>";
         tablaMensaje+="</tr>";
     }
     tablaMensaje+="</table>";
     $("#resultadoMensaje").append(tablaMensaje);
 }
 
-function editarInformacion(){
-    let myData={
-        id:$("#id").val(),
-        name:$("#name").val(),
-        email:$("#email").val(),
-        age:$("#age").val(),
+function mostrarRespuestaHabitaciones(items) {
+    let myTable = "<table>"
+    for (i = 0; i < items.length; i++) {
+        myTable+= "<tr>";
+        myTable+= "<td>" + items[i].id +"</td>";
+        myTable+= "<td>" + items[i].room +"</td>";
+        myTable+= "<td>" + items[i].stars +"</td>";
+        myTable+= "<td>" + items[i].category_id+"</td>";
+        myTable+= "<td>" + items[i].description+"</td>";
+        //myTable+= "<td> <button onclick='borrarHabitacion(" + items[i].id + ")'>Borrar</button>";
+        myTable+= "</tr>";
+    }
+    myTable += "</table>";
+    $("#resultadoHabitacion").append(myTable);
+}
 
-    };
-    console.log(myData);
-    let dataToSend=JSON.stringify(myData);
+function consultaInfoHabitacion() {
     $.ajax({
-        url:"https://g2ea280c58733a1-dbgrupo281111.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/client/client",
-        type:"PUT",
-        data:dataToSend,
-        contentType:"application/JSON",
+        url: "https://g2ea280c58733a1-dbgrupo281111.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/room/room",
+        type: "GET",
+        datatype: "JSON",
+        success: function(resultadoHabitacion) {
+            console.log(resultadoHabitacion);
+			$("#resultadoHabitacion").empty();
+            $("#id2").val(""),
+            $("#room").val(""),
+            $("#stars").val(""),
+            $("#category").val(""),
+            $("#description").val(""),
+            mostrarRespuestaHabitaciones(resultadoHabitacion.items);
+        }
+    });
+}
+
+function registrarHabitacion(){
+    let Datos={
+        id:$("#id2").val(),
+        room:$("#room").val(),
+        stars:$("#stars").val(),
+        category_id:$("#category").val(),
+        description:$("#description").val()
+    };
+    let dataToSend=JSON.stringify(Datos);
+    $.ajax({
+        url:"https://g2ea280c58733a1-dbgrupo281111.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/room/room",
+        type:"POST",
+        data:Datos,
         datatype:"JSON",
-        success:function(respuesta){
-            $("#resultado").empty();
-            $("#id").val("");
-            $("#name").val("");
-            $("#email").val("");
-            $("#age").val("");
-            consultaInfoCliente();
-            alert("se ha Actualizado")
+        success:function(resultadoHabitacion){
+            $("#resultadoHabitacion").empty();
+            $("#id2").val("");
+            $("#room").val("");
+            $("#stars").val("");
+            $("#category").val("");
+            $("#description").val("");
+            consultaInfoHabitacion();
+            alert("Se ha registro la habitacion")
         }
     });
 }
